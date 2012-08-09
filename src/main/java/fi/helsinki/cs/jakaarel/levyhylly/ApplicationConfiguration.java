e02a5bbb9174485538d3f228fa3e9612d0197ed0
killnine <killnine@8cc7abb3-f649-0410-a1f9-e0a067834fb0>
2012-08-09 12:43:29 +0000

    Dabbling with Spring/web configuration.

--- src/main/java/fi/helsinki/cs/jakaarel/levyhylly/ApplicationConfiguration.java
+++ src/main/java/fi/helsinki/cs/jakaarel/levyhylly/ApplicationConfiguration.java
@@ -1,8 +1,13 @@
 package fi.helsinki.cs.jakaarel.levyhylly;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Programmatic Spring Framework context configuration.
@@ -11,6 +16,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @EnableWebMvc
 @Configuration
+@ComponentScan(basePackages = "fi.helsinki.cs.jakaarel.levyhylly")
 public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean 
+	public InternalResourceViewResolver configureInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable(); 
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 
 }
