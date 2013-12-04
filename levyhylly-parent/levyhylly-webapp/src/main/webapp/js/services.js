6f4407c067f1e645488a5e57100dc2ca2b020d7e
Jani Kaarela <jani.kaarela@saunalahti.com>
2013-12-04 23:48:23 +0200

    Make ng resources host-agnostic by using $location.

--- levyhylly-parent/levyhylly-webapp/src/main/webapp/js/services.js
+++ levyhylly-parent/levyhylly-webapp/src/main/webapp/js/services.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var servicesModule = angular.module('levyhylly.services', ['ngResource']);
-servicesModule.service('Track', function($resource) {
-  return $resource('http://localhost\\:8080/albums/:albumId/tracks/:trackNumber', { albumId: '@albumId', trackNumber: '@number' });
+servicesModule.service('Track', function($resource,$location) {
+  return $resource($location.protocol() + '://' + $location.host() + '\\:8080/albums/:albumId/tracks/:trackNumber', { albumId: '@albumId', trackNumber: '@number' });
 });
